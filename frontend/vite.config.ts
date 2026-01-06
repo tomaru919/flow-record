bcb115a3dd0f8af6a206aece147da6447a9097a6
tomaru919 <kyukurarin919@gmail.com>
2026-01-06 21:45:17 +0900

    フロントエンドのビルドターゲットを削除し、vite.config.tsにビルド出力先を設定

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '../FlowRecord/bin/Release/net10.0-windows/win-x64/publish/wwwroot',
+    emptyOutDir: true,
+  }
 })
